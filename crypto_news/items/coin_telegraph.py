e1c6563177f1bfe9dc888d7fb4dfa476515764fc
AndriiSkavreniuk <skavreniuk@gmail.com>
2021-02-18 22:20:30 +0200

    changed рфтвдштп exсeption

--- crypto_news/items/coin_telegraph.py
+++ crypto_news/items/coin_telegraph.py
@@ -4,6 +4,10 @@ from itemloaders.processors import Join, MapCompose, TakeFirst
 
 
 class CoinTelegraphNews(scrapy.Item):
+    main_url = scrapy.Field(input_processor=MapCompose(remove_tags),
+                            output_processor=Join(), )
+    name_of_group = scrapy.Field(input_processor=MapCompose(remove_tags),
+                                 output_processor=Join(), )
     title = scrapy.Field(input_processor=MapCompose(remove_tags),
                          output_processor=Join(), )
     author = scrapy.Field(input_processor=MapCompose(remove_tags),
--- crypto_news/spiders/cointelegraph_news.py
+++ crypto_news/spiders/cointelegraph_news.py
@@ -1,8 +1,11 @@
 import scrapy
-from scrapy.exceptions import CloseSpider
-
+from scrapy.exceptions import IgnoreRequest
 from crypto_news.items import CoinTelegraphNews
 from datetime import datetime, timedelta
+
+const_form_data = {
+
+}
 
 
 class CointelegraphNewsSpider(scrapy.Spider):
@@ -12,32 +15,25 @@ class CointelegraphNewsSpider(scrapy.Spider):
     def parse(self, response, **kwargs):
         category_list = response.css(
             "li.menu-desktop__item div.menu-desktop-sub__list-wrp a::attr(href)").re(r'tags/\w+')
-        yield from response.follow_all(category_list, self.parse_list_news)
+        yield from response.follow_all(category_list,
+                                       self.parse_list_news_links, )
 
-    # def parse(self, response, **kwargs):
-    #     tmp = response.css("li.menu-desktop__item div.menu-desktop-sub__list-wrp a::attr(href)").re(r'tags/\w+')
-    #     for href in tmp:
-    #         yield response.follow(response.urljoin(href), self.parse_list_news)
-
-    def parse_list_news(self, response):
+    def parse_list_news_links(self, response):
+        name_of_group = response.css()
         list_of_news = response.css(
             "div.tag-page ul.posts-listing__list a.post-card-inline__figure-link::attr(href)").getall()
-        yield from response.follow_all(list_of_news, self.parse_news)
+        yield from response.follow_all(list_of_news,
+                                       self.parse_news,
+                                       meta={'name_of_group': name_of_group})
 
-    # def parse_list_news(self, response):
-    #     tmp = response.css(
-    #             "div.tag-page ul.posts-listing__list a.post-card-inline__figure-link::attr(href)").getall()
-    #     for href in tmp:
-    #         yield scrapy.Request(response.urljoin(self.start_urls[0], href), self.parse_news)іс
-    # response.css('div.post div.post-page__article div.post-meta div.post-meta__author-name').get()
 
     def parse_news(self, response):
         news = CoinTelegraphNews()
         news['title'] = response.css("div.post article.post__article h1.post__title::text")
         news['text'] = response.css("div.post article.post__article div.post-content").getall()
         news['date'] = datetime.strptime(response.css("div.post article.post__article time::attr(datetime)").get(),
-                                     '%Y-%m-%d')
+                                         '%Y-%m-%d')
         news['author'] = response.css("div.post article.post__article div.post-meta__author-name::text").get()
         if datetime.today() - news['date'] > timedelta(6):
-            raise CloseSpider('')
+            raise IgnoreRequest('all new in this date range parsed')
         yield news
