f89a43dc586fd0ca91db8932a7da94b09065b7ba
AndriiSkavreniuk <skavreniuk@gmail.com>
2021-02-19 19:03:14 +0200

    add info to json

--- crypto_news/spiders/cointelegraph_news.py
+++ crypto_news/spiders/cointelegraph_news.py
@@ -11,6 +11,7 @@ const_form_data = {
 class CointelegraphNewsSpider(scrapy.Spider):
     name = 'Cointelegraph_News'
     start_urls = ['https://cointelegraph.com//']
+    infinite_scroll_url = ['https://conpletus.cointelegraph.com/v1/']
 
     def parse(self, response, **kwargs):
         category_list = response.css(
@@ -19,7 +20,8 @@ class CointelegraphNewsSpider(scrapy.Spider):
                                        self.parse_list_news_links, )
 
     def parse_list_news_links(self, response):
-        name_of_group = response.css()
+        name_of_group = str(response).split('/')[-1]
+        name_of_group = name_of_group[:-1]
         list_of_news = response.css(
             "div.tag-page ul.posts-listing__list a.post-card-inline__figure-link::attr(href)").getall()
         yield from response.follow_all(list_of_news,
@@ -27,13 +29,18 @@ class CointelegraphNewsSpider(scrapy.Spider):
                                        meta={'name_of_group': name_of_group})
 
 
+
+
     def parse_news(self, response):
         news = CoinTelegraphNews()
-        news['title'] = response.css("div.post article.post__article h1.post__title::text")
+        news['main_url'] = self.start_urls[0]
+        news['name_of_group'] = response.meta['name_of_group']
+        news['title'] = response.css("div.post article.post__article h1.post__title::text").get()
         news['text'] = response.css("div.post article.post__article div.post-content").getall()
-        news['date'] = datetime.strptime(response.css("div.post article.post__article time::attr(datetime)").get(),
-                                         '%Y-%m-%d')
+        news['date'] = response.css("div.post article.post__article time::attr(datetime)").get()
         news['author'] = response.css("div.post article.post__article div.post-meta__author-name::text").get()
-        if datetime.today() - news['date'] > timedelta(6):
+        tmp = datetime.strptime(news['date'], '%Y-%m-%d')
+        if datetime.today() - tmp > timedelta(30):
             raise IgnoreRequest('all new in this date range parsed')
+
         yield news
