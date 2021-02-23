bb8c18fde45fcb8f8cb4476dc82f8d26e6422517
oleh_polytskyi <kopobai4uk@gmail.com>
2021-02-23 22:24:58 +0200

    finoshed crtpto news spider

--- crypto_news/spiders/crypto_news_spider.py
+++ crypto_news/spiders/crypto_news_spider.py
@@ -1,9 +1,9 @@
 import scrapy
-import chompjs
 import lxml.html
 import datetime
 from crypto_news.items import CryptoNewsItem
-import pdb
+import demjson
+
 const_form_data = {
     "event": "articles.articles#morepages",
     "where": None,
@@ -24,7 +24,6 @@ class CryptoNewsSpider(scrapy.Spider):
                                        self.parse_list_of_news)
 
     def parse_list_of_news(self, response):
-        pdb.set_trace()
         links_of_news_links = response.xpath('//div[contains(@class,'
                                              '"cn-tile article")]'
                                              '/a/@href').getall()
@@ -34,23 +33,25 @@ class CryptoNewsSpider(scrapy.Spider):
                                                   '/i/time/@datetime').getall()
         if response.xpath('//script/text()').re(".*_load_more.*"):
             script_function = response.xpath('//script/text()').re(".*_load_more.*")[0]
-            js_dict = chompjs.parse_js_object(script_function)
+            js_dict = demjson.decode(script_function[20:])
             formatted_data_form_script = lxml.html.fromstring(js_dict['pages'][0])
-            links_of_news_links.extend(formatted_data_form_script.xpath('//div/a/@href'))
-            list_of_date_posted_news.extend(formatted_data_form_script.xpath('//time/@datetime'))
-
-        form_data = const_form_data.copy()
-        form_data['where'] = js_dict['where']
+            links_of_news_links.extend(
+                formatted_data_form_script.xpath('//div/a/@href'))
+            list_of_date_posted_news.extend(
+                formatted_data_form_script.xpath('//time/@datetime'))
 
         for counter in range(0, len(links_of_news_links)-1):
             self.data_filter(list_of_date_posted_news[counter])
             yield scrapy.Request(self.start_urls[0][:-1]
                                  + links_of_news_links[counter], self.parse_news)
 
-        yield scrapy.FormRequest(url=response.url,
-                                 formdata=form_data,
-                                 callback=self.parse_list_of_news_load_more,
-                                 meta={'form_data': form_data})
+        if response.xpath('//div[contains(@class, "cn-section-controls")]'):
+            form_data = const_form_data.copy()
+            form_data['where'] = js_dict['where']
+            yield scrapy.FormRequest(url=response.url,
+                                     formdata=form_data,
+                                     callback=self.parse_list_of_news_load_more,
+                                     meta={'form_data': form_data})
 
     def parse_list_of_news_load_more(self, response):
         form_data = response.meta('form_data')
@@ -66,7 +67,6 @@ class CryptoNewsSpider(scrapy.Spider):
                                      self.parse_news)
 
     def data_filter(self, date):
-        # pdb.set_trace()
         if (datetime.datetime.now() - datetime.datetime.fromisoformat(date)
                 .replace(tzinfo=None)).days > int(self.days):
             raise scrapy.exceptions.IgnoreRequest('data incorrect')
@@ -80,9 +80,17 @@ class CryptoNewsSpider(scrapy.Spider):
         news_item['title'] = response.xpath('//article[contains(@class,'
                                             '"cn-article-page")]/h1/text()').get()
         news_item['author'] = []
-        news_item['author'].extend(response.xpath('//div[contains(@class,"cn-props-panel")]'
-                                              '/div[contains(@class,"author")]'
-                                              '/span/a/text()').get())
+        if response.xpath('//div[contains(@class,"cn-props-panel")]'
+                          '/div[contains(@class,"author")]/span/text()').get():
+            news_item['author'].append(
+                response.xpath('//div[contains(@class,"cn-props-panel")]'
+                               '/div[contains(@class,"author")]'
+                               '/span/text()').get())
+        else:
+            news_item['author'].append(
+                response.xpath('//div[contains(@class,"cn-props-panel")]'
+                               '/div[contains(@class,"author")]'
+                               '/span/a/text()').get())
 
         news_item['date'] = response.xpath('//div[contains(@class,"cn-props-panel")]'
                                            '/div[contains(@class,"time")]'
