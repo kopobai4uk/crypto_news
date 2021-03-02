6c6a90e46c37db99588e13fd40339b358692fc9e
oleh_polytskyi <kopobai4uk@gmail.com>
2021-03-02 11:52:24 +0200

    refactored code

--- crypto_news/items/cryptoslate_item.py
+++ crypto_news/items/cryptoslate_item.py
@@ -1,20 +1,11 @@
 import scrapy
-from w3lib.html import remove_tags
-from itemloaders.processors import Join, MapCompose, TakeFirst
 
 
 class CryptoSlateItem(scrapy.Item):
-    main_url = scrapy.Field(input_processor=MapCompose(remove_tags),
-                            output_processor=Join(),)
-    name_of_group = scrapy.Field(input_processor=MapCompose(remove_tags),
-                                 output_processor=Join(),)
-    name_of_subgroup = scrapy.Field(input_processor=MapCompose(remove_tags),
-                                    output_processor=Join(),)
-    title = scrapy.Field(input_processor=MapCompose(remove_tags),
-                         output_processor=Join(),)
-    authors = scrapy.Field(input_processor=MapCompose(remove_tags),
-                           output_processor=Join(),)
-    date = scrapy.Field(input_processor=MapCompose(remove_tags),
-                        output_processor=Join(),)
-    text = scrapy.Field(input_processor=MapCompose(remove_tags),
-                        output_processor=Join(),)
+    main_url = scrapy.Field()
+    name_of_group = scrapy.Field()
+    name_of_subgroup = scrapy.Field()
+    title = scrapy.Field()
+    authors = scrapy.Field()
+    date = scrapy.Field()
+    text = scrapy.Field()
